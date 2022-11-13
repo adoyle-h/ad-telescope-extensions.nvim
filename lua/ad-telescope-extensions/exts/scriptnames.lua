dcaabea92e6ac025c904d876e9d22000428cd6ff
ADoyle <adoyle.h@gmail.com>
2022-11-14 07:53:24 +0800

    fix(scriptnames): set cat previewer

--- lua/ad-telescope-extensions/exts/scriptnames.lua
+++ lua/ad-telescope-extensions/exts/scriptnames.lua
@@ -1,9 +1,15 @@
 return function(opts)
 	return {
 		desc = 'List scriptnames.',
+
+		picker = { previewer = 'cat' },
+
 		command = function()
 			local output = vim.api.nvim_exec('scriptnames', true)
-			return vim.split(output, '\n')
+			return vim.tbl_map(function(text)
+				local _, _, path = string.find(text, '^%s*%d+: (.+)')
+				return { text = text, entry = { path = path } }
+			end, vim.split(output, '\n'))
 		end,
 	}
 end
